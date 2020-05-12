da6bc9a04318b4bcc7a1dd3bbedb4edad16f90bf
xenock <miguelmoracho@gmail.com>
2020-05-12 19:11:54 +0200

    adding races and testing select

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,27 @@
 <script>
     import Input from './components/Input.svelte'
+    const races = [
+        {
+            id: 1,
+            text: 'Elfo'
+        },
+        {
+            id: 2,
+            text: 'Enano'
+        },
+        {
+            id: 3,
+            text: 'Halfling'
+        },
+        {
+            id: 4,
+            text: 'Humano'
+        },
+    ]
+
+    let selectedRace
+
+
 </script>
 
 <main>
@@ -9,7 +31,14 @@
 
         <Input fieldName={'name'} label={'Nombre'} />
 
-        <Input fieldName={'race'} label={'Raza'} />
+        <label for="race-field">Raza seleccionada: {selectedRace ? selectedRace.text : ''} </label>
+        <select id="race-field" name="race" bind:value={selectedRace}>
+            {#each races as race}
+                <option value={race}>
+                    {race.text}
+                </option>
+            {/each}
+        </select>
 
         <Input fieldName={'actual-career'} label={'Profesión actual'} />
 
