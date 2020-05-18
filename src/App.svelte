bea172741dceb4ea7aba09430ede601f6ed01728
xenock <miguelmoracho@gmail.com>
2020-05-18 18:02:05 +0200

    feat: added characteristics fields

--- src/App.svelte
+++ src/App.svelte
@@ -44,4 +44,40 @@
 
         <Input fieldName={'former-career'} label={'Profesión anterior'} />
     </fieldset>
+
+    <fieldset>
+        <legend>Detalles personales</legend>
+    </fieldset>
+
+    <fieldset>
+        <legend>Perfil del personaje</legend>
+
+        <fielset>
+            <legend>Primarias</legend>
+
+            <fielset>
+                <legend>Iniciales</legend>
+                <Input fieldName={'weaponSkill'} label={'HA'} />
+                <Input fieldName={'ballisticSkill'} label={'HP'} />
+                <Input fieldName={'strength'} label={'F'} />
+                <Input fieldName={'toughness'} label={'R'} />
+                <Input fieldName={'agility'} label={'Ag'} />
+                <Input fieldName={'intelligence'} label={'I'} />
+                <Input fieldName={'willPower'} label={'V'} />
+                <Input fieldName={'fellowship'} label={'Em'} />
+            </fielset>
+        </fielset>
+
+        <fielset>
+            <legend>Secundarias</legend>
+            <Input fieldName={'attacks'} label={'A'} />
+            <Input fieldName={'wounds'} label={'H'} />
+            <Input fieldName={'strengthBonus'} label={'BF'} />
+            <Input fieldName={'toughnessBonus'} label={'BR'} />
+            <Input fieldName={'movement'} label={'M'} />
+            <Input fieldName={'magic'} label={'Mag'} />
+            <Input fieldName={'insanityPoints'} label={'PL'} />
+            <Input fieldName={'fatePoints'} label={'PD'} />
+        </fielset>
+    </fieldset>
 </main>
